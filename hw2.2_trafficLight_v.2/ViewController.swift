a0badf4c3650a311823e09902fb33a4516d086f5
vvduniverse <86676363+vvduniverse@users.noreply.github.com>
2021-07-08 16:48:53 +0300

    Initial Commit

---
+++ hw2.2_trafficLight_v.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw2.2_trafficLight_v.2
+//
+//  Created by Tato Admin on 08.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
